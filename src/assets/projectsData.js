601cc1b0b23540b963a567915377802aad839b2b
Bhekisile <bhekisilejozi@gmail.com>
2023-12-18 11:36:12 +0200

    Add portfolio project images

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -2,18 +2,45 @@ const projectsData = [
   {
     projectName: 'College of Science, Engineering and Technology 2023',
     projectDescription: 'The project is about the College of Science Department at the University of South Africa. The navigation bar is the feature I like the most; it takes you to the section you want. There are courses, lectures, and sponsorship sections.',
-    image: 'capsHome.png',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/capsHome.png?raw=true',
     projectUrl: 'https://github.com/Bhekisile/capstone-project/',
     projectIndex: 'projectIndex1',
   },
   {
     projectName: 'Awesome Books',
     projectDescription: 'This is a library project that allows the addition and removal of books. It uses the Luxon library to display dates and times. It is a website that contains add-on books, book lists, and contact details pages.',
-    image: './ImageP1/awesomeBook.png',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/awesomeBook.png?raw=true',
     projectUrl: 'https://github.com/Bhekisile/Awesome_books',
     projectIndex: 'projectIndex2',
   },
-
+  {
+    projectName: 'To Do List',
+    projectDescription: 'To-do-list is a list of tasks that need to be completed. The user can add, edit, and delete the task and each task is rendered dynamically on page load. The project uses webpack to bundle JavaScript files.',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/awesomeBook.png?raw=true',
+    projectUrl: 'https://github.com/Bhekisile/To-do-list',
+    projectIndex: 'projectIndex3',
+  },
+  {
+    projectName: 'Recipe App',
+    projectDescription: 'This project will help you keep track of all your recipes and ingredients. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/awesomeBook.png?raw=true',
+    projectUrl: 'https://github.com/Bhekisile/Recipe-app',
+    projectIndex: 'projectIndex4',
+  },
+  {
+    projectName: 'Budget App',
+    projectDescription: 'This is a mobile web application where you can manage your budget. There is a list of transactions associated with the category, so that you can see how much money you spent and on what.',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/awesomeBook.png?raw=true',
+    projectUrl: 'https://github.com/Bhekisile/budget-app',
+    projectIndex: 'projectIndex5',
+  },
+  {
+    projectName: 'Stock Screener',
+    projectDescription: 'This is a metric website containing data or parameters used for stock screening. It displays a list of companies from all over the world with the corresponding parameters and values. The user can filter the application by company\'s name. It is more beneficial to those who search for stocks. The data is extracted from the Financial Modeling Prep API.',
+    imageUrl: 'https://github.com/Bhekisile/Portfolio-React/blob/portfolio/src/assets/awesomeBook.png?raw=true',
+    projectUrl: 'https://github.com/Bhekisile/Metrics-webapp',
+    projectIndex: 'projectIndex6',
+  },
 ];
 
 export default projectsData;
